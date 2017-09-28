0b755bbad9dc20adf5c7de9a0631bfe242920da0
Justin Kim <jjkim928@gmail.com>
2017-09-28 16:33:41 -0400

    changed to customerService

--- src/main/java/com/example/customer/controller/CustomerController.java
+++ src/main/java/com/example/customer/controller/CustomerController.java
@@ -1,20 +1,27 @@
 package com.example.customer.controller;
 
-import com.example.customer.repository.CustomerRepository;
 import com.example.customer.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CustomerController {
 
 
+    @Autowired
     private final CustomerService customerService;
 
-    @Autowired
     public CustomerController(CustomerService customerService) {
         this.customerService = customerService;
     }
 
-    
+    @GetMapping("/")
+    public String listAllCustomers(Model model) {
+        model.addAttribute("customers", customerService.listAllCustomers());
+        return "all-customers";
+    }
+
+
 }
