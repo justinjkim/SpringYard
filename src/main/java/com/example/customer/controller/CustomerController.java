78d42a8e5aaded0514b641eeeb99c2e0e938a372
Jamie Bort <jamiebort@gmail.com>
2017-09-29 12:47:09 -0400

    added the 'add' feature

--- src/main/java/com/example/customer/controller/CustomerController.java
+++ src/main/java/com/example/customer/controller/CustomerController.java
@@ -1,10 +1,12 @@
 package com.example.customer.controller;
 
+import com.example.customer.model.Customer;
 import com.example.customer.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
@@ -21,6 +23,17 @@ public class CustomerController {
     public String listAllCustomers(Model model) {
         model.addAttribute("customers", customerService.listAllCustomers());
         return "all-customers";
+    }
+
+    @GetMapping("/customerForm")
+    public String customerForm(){
+        return "customerForm";
+    }
+
+    @PostMapping("/create-customer")
+    public String createCustomer(Customer customer){
+        customerService.createCustomer(customer);
+        return "redirect:/";
     }
 
 
--- src/main/java/com/example/customer/service/CustomerServiceImpl.java
+++ src/main/java/com/example/customer/service/CustomerServiceImpl.java
@@ -28,7 +28,7 @@ public class CustomerServiceImpl implements CustomerService {
 
     @Override
     public void createCustomer(Customer customer) {
-
+        customerRepository.createCustomer(customer);
     }
 
 
