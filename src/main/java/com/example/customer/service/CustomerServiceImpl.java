1ad4155d94df3a8a02b78d0637d88f45f07705a7
Justin Kim <jjkim928@gmail.com>
2017-09-29 00:01:04 -0400

    added @service annotation

--- src/main/java/com/example/customer/service/CustomerServiceImpl.java
+++ src/main/java/com/example/customer/service/CustomerServiceImpl.java
@@ -3,10 +3,12 @@ package com.example.customer.service;
 import com.example.customer.model.Customer;
 import com.example.customer.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Service
 public class CustomerServiceImpl implements CustomerService {
 
 
