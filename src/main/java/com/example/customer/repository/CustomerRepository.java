e6eb0def88bdbbfd5dbd677193c531e7b0e1082e
Justin Kim <jjkim928@gmail.com>
2017-10-02 15:24:39 -0400

    removed methods from interface

--- src/main/java/com/example/customer/repository/CustomerRepository.java
+++ src/main/java/com/example/customer/repository/CustomerRepository.java
@@ -6,8 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-    void createCustomer(Customer customer);
-    List<Customer> findCustomer(String search);
-    void updateCustomer(Customer customer);
-    void deleteCustomer(Customer customer);
+
 }
