4d469076a2f2e263a4cce513981d1935333177b2
Justin Kim <jjkim928@gmail.com>
2017-10-02 15:30:45 -0400

    changed createCustomer() to return Customer

--- src/main/java/com/example/customer/service/CustomerService.java
+++ src/main/java/com/example/customer/service/CustomerService.java
@@ -5,7 +5,7 @@ import com.example.customer.model.Customer;
 import java.util.List;
 
 public interface CustomerService {
-    void createCustomer(Customer customer);
+    Customer createCustomer(Customer customer);
     List<Customer> findCustomer(String find);
     void updateCustomer(Customer customer);
     void deleteCustomer(Customer customer);
