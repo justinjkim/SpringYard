07655f2c939ac4812ed9bf1f53082e5d93691e09
Justin Kim <jjkim928@gmail.com>
2017-10-02 15:22:23 -0400

    extension of JpaRepository...also deleted the customerRepositoryImpl

--- src/main/java/com/example/customer/repository/CustomerRepository.java
+++ src/main/java/com/example/customer/repository/CustomerRepository.java
@@ -1,10 +1,11 @@
 package com.example.customer.repository;
 
 import com.example.customer.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface CustomerRepository {
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
     void createCustomer(Customer customer);
     List<Customer> findCustomer(String search);
     void updateCustomer(Customer customer);
