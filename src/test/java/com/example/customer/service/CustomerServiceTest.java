2334dd58e88348f7586d96e4db057a439213321d
Justin Kim <jjkim928@gmail.com>
2017-10-01 23:45:11 -0400

    added listAllCustomersTest()

--- src/test/java/com/example/customer/service/CustomerServiceTest.java
+++ src/test/java/com/example/customer/service/CustomerServiceTest.java
@@ -1,21 +1,34 @@
 package com.example.customer.service;
 
+import com.example.customer.model.Customer;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CustomerServiceTest {
 
     @Autowired
-    CustomerService customerService;
+    CustomerService customerServiceImpl;
 
     @Test
-    public void listAllCustomersTest() {
+    public void findAllCustomersTest() {
+        //arrange
+
+        //act
+        List<Customer> customers = customerServiceImpl.findCustomer("");
+
+        //assert
+        assertEquals(13, customers.size());
 
     }
+
 
 }
