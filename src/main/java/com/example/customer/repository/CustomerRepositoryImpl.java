7317f9e2e497d17ddacdb0538f657484e8c73048
Justin Kim <jjkim928@gmail.com>
2017-10-01 23:42:41 -0400

    tried adding SQL method for updating, but not sure how to actually write it out correctly...

--- src/main/java/com/example/customer/repository/CustomerRepositoryImpl.java
+++ src/main/java/com/example/customer/repository/CustomerRepositoryImpl.java
@@ -19,27 +19,20 @@ public class CustomerRepositoryImpl implements CustomerRepository{
     }
 
     @Override
-    public List<Customer> listAllCustomers() {
-        return jdbcTemplate.query(
-                "SELECT firstname, lastname, phone, email FROM customer",
-                (resultSet, i) -> new Customer(resultSet.getString("firstname"), resultSet.getString("lastname"), resultSet.getString("phone"), resultSet.getString("email")));
-    }
-
-    @Override
     public void createCustomer(Customer customer) {
         jdbcTemplate.update("INSERT INTO customer (firstname, lastname, phone, email) VALUES (?, ?, ?, ?)", customer.getFirstName(), customer.getLastName(), customer.getPhone(), customer.getEmail());
     }
 
     @Override
     public List<Customer> findCustomer(String search) {
-        return jdbcTemplate.query("SELECT firstname, lastname, phone, email FROM customer WHERE (firstname = ? OR lastname = ? OR phone = ? OR email = ?) OR ? = '' ",
+        return jdbcTemplate.query("SELECT id, firstname, lastname, phone, email FROM customer WHERE (firstname = ? OR lastname = ? OR phone = ? OR email = ?) OR ? = '' ",
                 (resultSet, i) -> new Customer(resultSet.getString("firstname"), resultSet.getString("lastname"), resultSet.getString("phone"), resultSet.getString("email")),
                 search, search, search, search, search);
     }
 
     @Override
     public void updateCustomer(Customer customer) {
-
+        jdbcTemplate.update("UPDATE customer SET firstname = ?, lastname = ?, phone = ?, email = ? WHERE id = ?)", customer.getFirstName(), customer.getLastName(), customer.getPhone(), customer.getEmail());
     }
 
     @Override
