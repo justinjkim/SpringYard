bd8e4f408fbabd0004b2605c4bff4b9213f649ce
Justin Kim <jjkim928@gmail.com>
2017-10-01 23:43:08 -0400

    removed listAllCustomers() method

--- src/main/java/com/example/customer/service/CustomerService.java
+++ src/main/java/com/example/customer/service/CustomerService.java
@@ -5,7 +5,6 @@ import com.example.customer.model.Customer;
 import java.util.List;
 
 public interface CustomerService {
-    List<Customer> listAllCustomers();
     void createCustomer(Customer customer);
     List<Customer> findCustomer(String find);
     void updateCustomer(Customer customer);
